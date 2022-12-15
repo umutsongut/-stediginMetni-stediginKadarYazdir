2ffb5a06d5101e10234d20fd9a6e883e33b770c8
Umut Söngüt <1umut.songut@gmail.com>
2022-12-15 23:19:51 +0300

    Add project files.

---
+++ İstediginMetniİstediginKadarYazdir/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İstediginMetniİstediginKadarYazdir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string metin;
+            int deneme = 0;
+            int t;
+
+            Console.Write("Yazdırmak İstediğiniz Metni Girin: ");
+            metin = Console.ReadLine();
+
+            Console.Write("Metni Kaç Kez Tekrarlamak İstiyorsunuz?");
+            t = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= t; i++)
+            {
+                deneme++;
+                Console.WriteLine($"{deneme}.Deneme, {metin}");
+            }
+        }
+    }
+}
